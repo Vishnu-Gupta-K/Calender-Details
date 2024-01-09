7eb3157258ba0a32a1f0004b7ea5449f39f08dbb
Vishnu-Gupta-K <156089347+Vishnu-Gupta-K@users.noreply.github.com>
2024-01-09 20:14:55 +0530

    Add files via upload
    Hello,
    My name is Vishnu Gupta and I would like to share with you a small project that I have created. It is a calendar program that allows you to view and manage your events. I have used Java to write the code for this project, which is not very difficult but still demonstrates some of my skills. I hope you find it useful and interesting. Thank you for your attention.

---
+++ Calender.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Calender {
+    public static void main(String arg[]) {
+        Calendar c = Calendar.getInstance();
+        System.out.println("Date Is");
+        System.out.println(c.get(Calendar.DATE) + "/" + c.get(Calendar.MONTH) + "/" + c.get(Calendar.YEAR));
+	System.out.println(" Time Is ");
+        System.out.println(c.get(Calendar.HOUR) + "/" + c.get(Calendar.MINUTE) + "/" + c.get(Calendar.SECOND));
+    }
+}
